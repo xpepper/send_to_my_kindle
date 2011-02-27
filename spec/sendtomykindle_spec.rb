c1cb4f6b0e0607ca99ae6e8384cdaa653917793d
Pietro Di Bello <pietro.dibello@xpeppers.com>
2011-02-27 23:44:00 +0100

    moved from test to spec

---
+++ spec/sendtomykindle_spec.rb
@@ -0,0 +1,8 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+describe "Send to my kindle" do
+  it "fails" do
+    true.should be_true
+    # fail "hey buddy, you should probably rename this file and start specing for real"
+  end
+end
